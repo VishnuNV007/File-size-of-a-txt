fd9ddfe8589b7a6f68c6966dca48b7552167430e
Vishnu Narayanan V <100830902+VishnuNV007@users.noreply.github.com>
2022-08-11 19:57:41 +0530

    Add files via upload

---
+++ filesize.py
@@ -0,0 +1,10 @@
+#Python program to get the file size of a plain file
+
+
+
+def file_size(fname):
+        import os
+        statinfo = os.stat(fname)
+        return statinfo.st_size
+
+print("File size in bytes of a plain file: ",file_size("python1.txt"))
